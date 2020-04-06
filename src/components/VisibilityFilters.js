dab743d7979f5b151df88c9dd1122e0d379c68ca
Ally Brannon <allison.brannon@gmail.com>
2020-04-06 13:29:32 -0400

    fixed visibility of completed and incomplete

--- src/components/VisibilityFilters.js
+++ src/components/VisibilityFilters.js
@@ -1,10 +1,10 @@
 import React from "react";
 import cx from "classnames";
-import { VISIBILITY_FILTERS } from "../constants";
 import { connect } from "react-redux";
 import { setFilter } from "../redux/actions";
+import { VISIBILITY_FILTERS } from "../constants";
 
-const VisibilityFilters = ({ activeFilter }) => {
+const VisibilityFilters = ({ activeFilter, setFilter }) => {
   return (
     <div className="visibility-filters">
       {Object.keys(VISIBILITY_FILTERS).map((filterKey) => {
@@ -16,7 +16,9 @@ const VisibilityFilters = ({ activeFilter }) => {
               "filter",
               currentFilter === activeFilter && "filter--active"
             )}
-            onClick={() => {} /** waiting for setFilter handler*/}
+            onClick={() => {
+              setFilter(currentFilter);
+            }}
           >
             {currentFilter}
           </span>
@@ -29,5 +31,5 @@ const VisibilityFilters = ({ activeFilter }) => {
 const mapStateToProps = (state) => {
   return { activeFilter: state.visibilityFilter };
 };
-
+// export default VisibilityFilters;
 export default connect(mapStateToProps, { setFilter })(VisibilityFilters);
